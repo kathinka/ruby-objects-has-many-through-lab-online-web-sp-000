dfc9ae510b65a178cbaf1d1a17766e84373c70ed
kathinka martinsen <kathinkamartinsen@gmail.com>
2019-04-03 15:02:32 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -14,4 +14,10 @@ class Patient
   def new_appointment(date, doctor)
     Appointment.new(date, self, doctor)
   end
+
+  def appointments
+    Appointment.all.select do |appointment|
+      appointment.patient == self
+    end
+  end
 end
