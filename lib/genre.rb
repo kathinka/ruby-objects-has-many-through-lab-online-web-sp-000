f4caf7d6c0935ec02afb581358a0410ee931af00
kathinka martinsen <kathinkamartinsen@gmail.com>
2019-04-03 14:12:58 +0000

    Automatically backed up by Learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,23 +1,24 @@
 class Genre
-attr_accessor :song, :artist, :name
-@@all = []
+  attr_accessor :song, :artist, :name
+  @@all = []
 
-def initialize(name)
-@name = name
-@@all << self
-end
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
 
-def self.all
-@@all
-end
+  def self.all
+    @@all
+  end
 
-def name
-@name
-end
+  def name
+    @name
+  end
 
-def genres
-Song.map do |songs|
-  song.genre
-end
+  def genres
+    Song.map do |songs|
+      song.genre
+    end
+  end
 
 end
