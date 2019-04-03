8b56f37108ff7cb2b64cb6248aef33eade9c5401
kathinka martinsen <kathinkamartinsen@gmail.com>
2019-04-03 15:35:03 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -16,15 +16,15 @@ class Patient
   end
 
   def appointments
-    Appointment.all.select do |appointment, doctor|
+    Appointment.all.select do |appointment|
       appointment.patient == self
-      puts appointment.doctor
+      #puts appointment.doctor
     end
   end
 
   def doctors
-    appointments.map do |appointment, doctor|
-      appointment.doctor.name
+    appointments.map do |appointment|
+      appointment.doctor
     end
   end
 end
