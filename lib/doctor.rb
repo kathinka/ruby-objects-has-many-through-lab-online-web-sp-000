47ba14712761c269412c4505a69e84cf76927f3a
kathinka martinsen <kathinkamartinsen@gmail.com>
2019-04-03 15:46:51 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -26,7 +26,7 @@ class Doctor
   #  Appointment.all.select do |appointment|
     #  appointment.doctor == self
     appointments.map do |appointment|
-      appointment.patients
+      appointment.patient
     end
   end
 
