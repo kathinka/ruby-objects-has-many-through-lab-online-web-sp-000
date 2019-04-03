9a2d548a2c2964215e2b9208ace23dff751ac9fa
kathinka martinsen <kathinkamartinsen@gmail.com>
2019-04-03 14:32:18 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -3,29 +3,28 @@ class Artist
   attr_reader :name
   @@all = []
 
-def initialize(name)
-  @name = name
-  @@all << self
-
-end
-
-def self.all
-@@all
-end
-
-def new_song(name, genre)
-  Song.new(name, self, genre)
-end
-
-def songs
-  Song.all.select do |song|
-    song.artist == self
+  def initialize(name)
+    @name = name
+    @@all << self
   end
-end
-def genres
-  songs.map do |song|
-    song.genre
+
+  def self.all
+  @@all
   end
-end
+
+  def new_song(name, genre)
+    Song.new(name, self, genre)
+  end
+
+  def songs
+    Song.all.select do |song|
+      song.artist == self
+    end
+  end
+  def genres
+    songs.map do |song|
+      song.genre
+    end
+  end
 
 end
